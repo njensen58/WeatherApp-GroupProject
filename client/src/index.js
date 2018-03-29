581fe033f00b525c5a9c9fa14b7edc601b20a3c2
Nate Jensen <nathan.jensen.58@gmail.com>
2018-03-29 10:21:50 -0600

    upToDateMAR28

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import App from './App';
 
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
-, document.getElementById('root'));
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
+document.getElementById('root'));
