1cfb88a56887c32364f660f134298492de1fdb20
Nate Jensen <nathan.jensen.58@gmail.com>
2018-03-29 09:05:03 -0600

    navbar

---
+++ client/src/Components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './navStyles.css';
+
+
+class Navbar extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Navbar;
