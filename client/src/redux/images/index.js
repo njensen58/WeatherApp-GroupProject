3f153fa50bc08243b52ec7c9f9b7a5e9860323af
Alex Orr <alexander.justin.orr@gmail.com>
2018-04-03 22:16:59 -0400

    image API work

--- client/src/redux/images/index.js
+++ client/src/redux/images/index.js
@@ -1,5 +1,32 @@
 import axios from 'axios';
+import dotenv from "dotenv";
+import config from "../../config.js";
+const imagesAxios = axios.create();
+dotenv.config();
 
+cosnt TOKEN = config.PIC_TOKEN;
 
+imagesAxios.interceptors.request.use(config => {
+    const token = TOKEN;
+    config.headers.Authorization = `Bearer ${token}`;
+    return config
+})
+
+export function getImage(city){
+    return dispatch => {
+        return imagesAxios.get().then()
+    }
+}
+
+function reducer(state = {}, action){
+    switch(action.type) {
+        case "GET_IMAGE":
+            return {
+                image: action.image
+            }
+        default:
+            return state;
+    }
+}
 
 export default reducer;
